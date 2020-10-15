40ed8a1ca10546125349907d7a726883eec13dbd
Commit <>
2020-10-15 10:59:24 +0200

    practica 3 completa

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 
 const ages = [20,10,16,64,17,97,43,21,18,50];
 console.log(ages);
-const MenorEdad = ages.filter((elem,index,arr) => {
-    return elem < 18;
+
+const SumaTodos = ages.reduce((total,elem,index,arr) => {
+    return total + elem ;
 });
 
-console.log('Solo menores de edad:', MenorEdad);
+
+
+console.log('Esta es la suma de todos:',SumaTodos);
