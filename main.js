a1ae942fcca474df871430c3ed8348bcc57b0616
Commit <>
2020-10-15 10:45:50 +0200

    Practica 2 resuelta

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
 const ages = [20,10,16,64,17,97,43,21,18,50];
 console.log(ages);
-const MayorEdad = ages.filter((elem,index,arr) => {
-    return elem >= 18;
+const MenorEdad = ages.filter((elem,index,arr) => {
+    return elem < 18;
 });
 
-console.log('Solo mayores de edad:', MayorEdad);
+console.log('Solo menores de edad:', MenorEdad);
