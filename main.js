aa24763067c6f109f0de107464fab800ca22aebf
Commit <>
2020-10-15 10:43:15 +0200

    Segundo ejercicio de la segunda practica

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
-const ages = [20,10,33,64,82,97,43,21,18,50];
+const ages = [20,10,16,64,17,97,43,21,18,50];
 console.log(ages);
-const Multiplicar = ages.map((elem,index,arr) => {
-    return elem * 4;
+const MayorEdad = ages.filter((elem,index,arr) => {
+    return elem >= 18;
 });
 
-console.log(Multiplicar);
+console.log('Solo mayores de edad:', MayorEdad);
